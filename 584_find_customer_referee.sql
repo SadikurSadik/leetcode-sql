e70440630b76de198331b2121d940fc9aff173a5
Sadik <sadik7070@gmail.com>
2022-07-15 11:35:33 +0400

    584. Find Customer Refree solved

--- 584_find_customer_referee.sql
+++ 584_find_customer_referee.sql
@@ -1,7 +1,7 @@
 /*******************************************************************************************
- 1757. Recyclable and Low Fat Products
- Problem Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
+ Find Customer Referee
+ Problem Link: https://leetcode.com/problems/find-customer-referee/
  Platform: MySql
 ********************************************************************************************/
 
-SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
+SELECT name FROM Customer WHERE referee_id IS NULL OR referee_id != 2;
